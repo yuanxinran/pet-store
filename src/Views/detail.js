855e5c155e3b1bb31d05fc5cde9965c97e910d57
Anna Yuan <annayuan@Annas-MBP.fios-router.home>
2018-10-07 00:00:08 -0400

    review

--- src/Views/detail.js
+++ src/Views/detail.js
@@ -2,13 +2,94 @@ import React, { Component } from "react";
 import "../styles/detail.css";
 import { getItem } from "../Components/item.js";
 
+function createStars(review) {
+  let result = "";
+  for (let i = 1; i <= review; i++) {
+    result += "★";
+  }
+  return result;
+}
+
+function createUnfilled(review) {
+  let result = "";
+  for (let i = review + 1; i <= 5; i++) {
+    result += "★";
+  }
+  return result;
+}
+
+class ReviewDetail extends Component {
+  constructor(props) {
+    super(props);
+  }
+  render() {
+    review = this.props.review;
+    return (
+      <div className="review-container">
+        <div className="content">
+          <div className="header">
+            <div className="average">
+              <span className="filled">{createStars(review)}</span>
+              <span className="unfilled">{createUnfilled(review)}</span>
+            </div>
+            <div className="review-cat" />
+          </div>
+          <div className="" />
+        </div>
+      </div>
+    );
+  }
+}
+
+class Counter extends Component {
+  constructor(props) {
+    super(props);
+  }
+  state = {
+    count: 1
+  };
+
+  increment = () => {
+    this.setState({ count: this.state.count + 1 });
+  };
+
+  decrement = () => {
+    let ori = this.state.count;
+    let countn = this.state.count == 1 ? ori : ori - 1;
+    this.setState({ count: countn });
+  };
+
+  render() {
+    let btnclass = "btn";
+    btnclass += this.state.count == 1 ? " noincrease" : "";
+
+    return (
+      <div className="counter">
+        <input
+          type="button"
+          className={btnclass}
+          onClick={this.decrement}
+          value="-"
+        />
+        <div className="count">{this.state.count}</div>
+        <input
+          type="button"
+          className="btn"
+          onClick={this.increment}
+          value="+"
+        />
+      </div>
+    );
+  }
+}
+
 class SizeSelect extends Component {
   constructor(props) {
     super(props);
   }
   render() {
     return (
-      <div class="size-select-container">
+      <div className="size-select-container">
         <button id="small">Small</button>
         <button id="medium">Medium</button>
         <button id="large">Large</button>
@@ -16,14 +97,42 @@ class SizeSelect extends Component {
     );
   }
 }
+
 class ColorSelect extends Component {
   state = {};
   render() {
     return (
-      <div class="color-select-container">
-        <button id="small">Small</button>
-        <button id="medium">Medium</button>
-        <button id="large">Large</button>
+      <div className="color-select-container">
+        <form>
+          <input
+            type="image"
+            src={require("../imgs/red.png")}
+            alt="Submit"
+            width="48"
+            height="48"
+          />
+          <input
+            type="image"
+            src={require("../imgs/blue.png")}
+            alt="Submit"
+            width="48"
+            height="48"
+          />
+          <input
+            type="image"
+            src={require("../imgs/navy.png")}
+            alt="Submit"
+            width="48"
+            height="48"
+          />
+          <input
+            type="image"
+            src={require("../imgs/yellow.png")}
+            alt="Submit"
+            width="48"
+            height="48"
+          />
+        </form>
       </div>
     );
   }
@@ -34,24 +143,6 @@ class Detail extends Component {
   constructor(props) {
     super(props);
     this.state.item = getItem(this.props.match.params.id);
-  }
-
-  createStars() {
-    let item = this.state.item;
-    let result = "";
-    for (let i = 1; i <= item.review; i++) {
-      result += "★";
-    }
-    return result;
-  }
-
-  createUnfilled() {
-    let item = this.state.item;
-    let result = "";
-    for (let i = item.review + 1; i <= 5; i++) {
-      result += "★";
-    }
-    return result;
   }
 
   render() {
@@ -77,14 +168,20 @@ class Detail extends Component {
           <div className="detail-description">
             <div className="title">{item.title}</div>
             <div className="review">
-              <span className="filled">{this.createStars()}</span>
-              <span className="unfilled">{this.createUnfilled()}</span>
+              <span className="filled">{createStars(item.review)}</span>
+              <span className="unfilled">{createUnfilled(item.review)}</span>
               <span className="review-num">{item.reviewNum} reviews</span>
             </div>
             <div className="price">{item.price}</div>
             <div className="size">
               <SizeSelect />
+              <ColorSelect />
             </div>
+            <Counter />
+          </div>
+          <div className="detail-info">
+            <div className="review-detail" />
+            <div className="description-detail" />
           </div>
         </div>
       </React.Fragment>
